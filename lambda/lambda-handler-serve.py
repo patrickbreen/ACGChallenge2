54030a75f40e92c44a7cc3c678816168666d7841
EC2 Default User <ec2-user@ip-172-31-23-191.ec2.internal>
2020-09-26 20:08:08 +0000

    recreate the serve lambda, and verify everything works.

--- lambda/lambda-handler-serve.py
+++ lambda/lambda-handler-serve.py
@@ -1,38 +1,24 @@
-# import os
-# import decimal
+import csv
+import os
+import json
 
-# import boto3
+import boto3
 
-
-# # Get the service resource.
-# dynamodb = boto3.resource('dynamodb')
-
-# # set environment variable
-# TABLE_NAME = os.environ['TABLE_NAME']
-# table = dynamodb.Table(TABLE_NAME)
-
-# # init counter if not already exists
-# try:
-#     table.put_item(Item={'id': 'counter','visitor_count': 0}, ConditionExpression='attribute_not_exists(id)')
-# except:
-#     pass
+from etl_module import extract_nyt, extract_jh, get_data, load, merge
 
 
-# def handler(event, context):
-    
-#     # use an update expression as an atomic counter
-#     item = table.update_item(
-#         Key={'id': 'counter'},
-#         UpdateExpression="set visitor_count = visitor_count + :val",
-#         ExpressionAttributeValues={':val': decimal.Decimal(1)},
-#         ReturnValues="UPDATED_NEW")
-#     return {
-#         'statusCode': 200,
-#         'body': {'visitor_count': item['Attributes']['visitor_count']},
-#     }
+# Get the service resource.
+dynamodb = boto3.resource('dynamodb')
+
+
+# set environment variable
+TABLE_NAME = os.environ['TABLE_NAME']
+table = dynamodb.Table(TABLE_NAME)
+
 
 def handler(event, context):
+    items = table.scan()['Items']
     return {
         'statusCode': 200,
-        'body': {'key': 'hello'},
+        'body': {'data': items},
     }
